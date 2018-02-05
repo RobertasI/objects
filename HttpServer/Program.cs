72f6a84ff0e00bcff8411bd7bb24313f1ec82653
RobertasI <ilginis.robertas@gmail.com>
2018-02-05 21:54:54 +0200

    Https Server and Initial App added

---
+++ HttpServer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpServer
+{
+    class Program
+    {
+        private static Listener listener;
+        static void Main(string[] args)
+        {
+            listener = new Listener(8080);
+            Console.WriteLine("Listening on port 8080 (http://localhost:8080/).");
+            Console.WriteLine("Remember to put the files you want to serve in the output folder of the project.");
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
